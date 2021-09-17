918f2d04bfc2bb891d192129e43bd646fdd0dd59
League Student <league@iMac-34.attlocal.net>
2021-09-16 18:24:53 -0700

    IntroToStack,TextUndoRedo:IP:

--- src/_00_IntroToStacks/_01_IntroToStack.java
+++ src/_00_IntroToStacks/_01_IntroToStack.java
@@ -5,31 +5,54 @@ import java.util.Stack;
 
 import javax.swing.JOptionPane;
 
+import org.omg.Messaging.SyncScopeHelper;
+
 public class _01_IntroToStack {
-    public static void main(String[] args) {
-        // 1. Create a Stack of Doubles
-        //    Don't forget to import the Stack class
 
-        // 2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
+	public static void main(String[] args) {
+		// 1. Create a Stack of Doubles
+		// Don't forget to import the Stack class
+		Stack<Double> doubles = new Stack<Double>();
 
-        // 3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
+		// 2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
 
-        // 4. Pop all the elements off of the Stack. Every time a double is popped that is
-        //    between the two numbers entered by the user, print it to the screen.
+		Random rand = new Random();
+		for (int i = 0; i < 100; i++) {
+			doubles.push(rand.nextDouble() * 100);
+		}
+		
+		// 3. Ask the user to enter in two numbers between 0 and 100, inclusive.
+		String str = JOptionPane.showInputDialog(null, "Enter in one number between 0-100(lower limit)");
+		String str2 = JOptionPane.showInputDialog(null, "Enter in another number between 0-100(upper limit)");
+		int num2 = Integer.parseInt(str2);
+		int num = Integer.parseInt(str);
+		
+		// 4. Pop all the elements off of the Stack. Every time a double is popped that
+		// is
+		// between the two numbers entered by the user, print it to the screen.
+		while (!doubles.isEmpty()) {
+			double dob = doubles.pop();
+			if (dob > num && dob < num2) {
+				
+				
+				System.out.println(dob);
 
+			}
 
-        // EXAMPLE:
-        // NUM 1: 65
-        // NUM 2: 75
+		}
 
-        // Popping elements off stack...
-        // Elements between 65 and 75:
-        // 66.66876846
-        // 74.51651681
-        // 70.05110654
-        // 69.21350456
-        // 71.54506465
-        // 66.47984807
-        // 74.12121224
-    }
+		// EXAMPLE:
+		// NUM 1: 65
+		// NUM 2: 75
+
+		// Popping elements off stack...
+		// Elements between 65 and 75:
+		// 66.66876846
+		// 74.51651681
+		// 70.05110654
+		// 69.21350456
+		// 71.54506465
+		// 66.47984807
+		// 74.12121224
+	}
 }
--- src/_00_IntroToStacks/_02_TextUndoRedo.java
+++ src/_00_IntroToStacks/_02_TextUndoRedo.java
@@ -1,9 +1,18 @@
 package _00_IntroToStacks;
 
-public class _02_TextUndoRedo {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class _02_TextUndoRedo implements ActionListener {
+	
     /* 
      * Create a JFrame with a JPanel and a JLabel.
      * 
+     *
      * Every time a key is pressed, add that character to the JLabel. It should
      * look like a basic text editor.
      * 
@@ -16,6 +25,24 @@ public class _02_TextUndoRedo {
      * pressed, the top Character is popped  off the Stack and added back to
      * the JLabel.
      */
-
+	public static void main(String[] args) {
+		
+		new _02_TextUndoRedo().run();
+	}
+	void run(){
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JLabel label = new JLabel();
+		frame.add(panel);
+		frame.setSize(300, 200);
+		frame.setVisible(true);
+	
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
